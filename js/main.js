55a3ce450ddaaf240e098e4cec026f9754ea3a25
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2022-01-06 16:22:30 +0100

    improved scroll-box animation

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@ const header = document.querySelector('.page-header');
 window.addEventListener('scroll', () => {
 
     /* SLIDE SECTIONS IN FROM THE SIDES */
-    const triggerBottom = window.innerHeight / 4 * 3;
+    const triggerBottom = window.innerHeight / 5 * 4;
     // const triggerTop = window.innerHeight / 5;
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
         // const boxBottom = box.getBoundingClientRect().bottom;
 
         if (boxTop < triggerBottom) {
+            /* setTimeout(() => { box.classList.add('show') }, 50); */
             box.classList.add('show');
         } else {
+            /* setTimeout(() => { box.classList.remove('show') }, 50); */
             box.classList.remove('show');
         }
         /* if (boxBottom < triggerTop) {
