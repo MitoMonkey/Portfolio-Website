a92622a306ef9f486c0d293b0567e516e3fb356a
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2022-01-18 15:47:08 +0100

    added read-more buttons to projects

--- js/project-modal.js
+++ js/project-modal.js
@@ -1,25 +1,13 @@
-/* let projects = [
-    {
-        ID: 'MoveX-React-Modal',
-        showBtnId: 'Show-MoveX-React-Modal'
-    },
-    {
-        ID: 'MoveX-Angular-Modal',
-        showBtnId: 'Show-MoveX-Angular-Modal'
-    }
-]; */
-
-const projects = ['MoveX-React-Modal', 'MoveX-Angular-Modal'];
+/* const projects = ['Portfolio', 'MoveX_Angular', 'MoveX_React', 'MoveX_API', 'MeetApp', 'CHATzam'];
 
 projects.forEach(project => {
     // Get the button that opens the modal
-    /* let btn = document.getElementById(project.showBtnId); */
-    let btn = document.querySelector('#Show-' + project);
+    let btn = document.querySelector('#Show-' + project + '-Modal');
 
     // When the user clicks on the button, open the modal
     btn.onclick = function () {
         // Get and display the modal
-        var modal = document.getElementById(project);
+        var modal = document.getElementById(project + '-Modal');
         modal.style.display = "block";
 
         // When the user clicks anywhere outside of the modal, close it (.modal is actually the overlay and does not include .modal-content)
@@ -36,12 +24,38 @@ projects.forEach(project => {
         });
 
         // Get the <span> element that closes the modal and close the modal onclick
-        /* let close = document.getElementById(project.closeBtnId); */
-        /* let close = document.getElementsByClassName('closeModal'); */
-        let close = document.querySelector('#' + project + ' .closeModal');
+        let close = document.querySelector('#' + project + '-Modal' + ' .closeModal');
         close.onclick = function () {
             modal.style.display = "none";
         }
     }
-});
+}); */
 
+const btns = document.querySelectorAll('.read-more');
+btns.forEach(btn => {
+    let project = '#' + btn.id;
+    let modal = document.querySelector(project + '_container');
+    // When the user clicks on the button, open the corresponding modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it (.modal is actually the overlay and does not include .modal-content)
+    modal.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    // hide the modal when user presses ESC key
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modal.style.display === "block") {
+            modal.style.display = "none";
+        }
+    });
+
+    // Get the <span> element that closes the modal and close the modal onclick
+    let close = document.querySelector(project + '_container' + ' .closeModal');
+    close.onclick = function () {
+        modal.style.display = "none";
+    }
+});
