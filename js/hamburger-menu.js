dd6f59587bb1bf0d928bdf0f0efbf1576f40ac26
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2022-01-24 12:36:00 +0100

    fixed and improved hamburger nav

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,7 +1,19 @@
-let hamburger = document.getElementById('hamburger');
-let nav = document.getElementById("main-nav");
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('.navigation-list');
 
 hamburger.addEventListener('click', function (e) {
-    e.preventDefault();
-    nav.classList.toggle('responsive');
+    /* e.preventDefault(); */
+
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+
+    /* nav.style.display = 'block';
+    nav.classList.toggle('responsive'); */
 });
+
+/* close the (responsive) nav menu when a link is clicked */
+const navLinks = document.querySelectorAll(".navigation-list__item");
+navLinks.forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}));
